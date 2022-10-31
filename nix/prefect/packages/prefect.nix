8021a12fb6c90b151ba014bbc589c05bf959df57
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-31 15:32:44 -0700

    fix wrapped

--- nix/prefect/packages/prefect.nix
+++ nix/prefect/packages/prefect.nix
@@ -8,7 +8,10 @@
   frontend = buildNpmPackage {
     name = "prefect-frontend";
     src = source.src + "/orion-ui";
-    npmBuild = "npm run build";
+    installPhase = "cp -r dist $out";
+    npmBuild = ''
+      npm run build
+    '';
   };
   requirements = machlib.mkPython rec {
     requirements = builtins.readFile (source.src + "/requirements.txt");
@@ -25,6 +28,9 @@ in
     preConfigure = ''
       sed -i 's|__module_path__ / "orion" / "ui"|"${frontend}"|' src/prefect/__init__.py
     '';
+    makeWrapperArgs = [
+      "--prefix PYTHONPATH : $PYTHONPATH"
+    ];
     meta = with lib; {
       description = "https://github.com/PrefectHQ/prefect/blob/master/requirements.txt";
       homepage = "https://github.com/PrefectHQ/prefect";
