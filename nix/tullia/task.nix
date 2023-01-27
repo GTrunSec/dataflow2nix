ea59718fc18f7130826a273d43162184f6ef4448
guangtao <gtrunsec@hardenedlinux.org>
2023-01-27 02:46:36 -0800

    Update

--- nix/tullia/task.nix
+++ nix/tullia/task.nix
@@ -7,7 +7,36 @@
     command = {inherit type text;};
   };
 in {
-  nix =
+    ci = {
+    config ? {},
+    pkgs,
+    ...
+  }:
+    cmd "shell" ''
+      echo Fact:
+      cat ${pkgs.writeText "fact.json" (builtins.toJSON (config.facts.push or ""))}
+      echo CI passed
+    ''
+    // {
+      after = [
+        "format-nix"
+        "update"
+      ];
+    };
+
+  update = {
+    pkgs,
+    config,
+    ...
+  }:
+    cmd "shell" "nix flake lock --update-input std"
+    // {
+      preset.nix.enable = true;
+      # mkdir -p /local/home for isolation
+      env.HOME = "/home";
+    };
+
+  format-nix =
     cmd "shell" "alejandra flake.nix"
     // {
       dependencies = [nixpkgs.alejandra];
