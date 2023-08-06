1d0bbb8847a7e4890d82cea7f182e564ba7a32e6
guangtao <gtrunsec@hardenedlinux.org>
2023-08-06 03:48:12 -0700

    chore: update locks

--- nix/prefect/arionComposes.nix
+++ nix/prefect/arionComposes.nix
@@ -7,12 +7,4 @@
   l = inputs.nixpkgs.lib // builtins;
 in {
   deploy = std.lib.dev.mkArion cell.arionProfiles.deploy;
-
-  e = cell.arionComposes.deploy.extendModules {
-    modules = [
-      ({config, ...}: {
-      })
-    ];
-  };
-  test = std.lib.dev.mkArion cell.arionProfiles.test;
 }
